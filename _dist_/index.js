0117c5b09be9ffac92c9aed1360a472c5903c23e
pattopilco <pattopilco@users.noreply.github.com>
2022-08-11 21:48:36 +0000

    deploy: edc8c33b8f0742428e2d4251708a7a479cf92a05

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,5 +1,3 @@
-console.log('Happy hacking :)------------')
-
 const url = "https://platzi-avo.vercel.app";
 
 //web api trae recuros desde cualquier sitio
@@ -14,30 +12,43 @@ const url = "https://platzi-avo.vercel.app";
 //                 })
 
 
-//#Usando  async await
+
+const formatPrice = (price) =>
+  new Intl.NumberFormat("en-EN", {
+    style: "currency",
+    currency: "USD",
+  }).format(price);
+
 const nombres = async ()=>{
     const response = await fetch(`${url}/api/avo`);
     const data = await response.json();
-
-    const appNode = document.querySelector('div#app')
-    console.log('-------------------------');
+    const appNode = document.getElementById('app')
     const listaElemento=[]
     data.data.forEach(
         item => {
             const container = document.createElement('div')
-            //crear elementos html
             const image = document.createElement('img')
+            image.className = "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6";
             image.src = `${url}${item.image}`
             const title = document.createElement('h2')
             title.textContent = item.name
+            title.className = "text-xl text-red-600"
             const price = document.createElement('div')
-            price.textContent = item.price
-            container.append( image, title, price)
+            price.className = "text-gray-600";
+            price.textContent = formatPrice(item.price)
+            const priceAndTitle = document.createElement("div");
+            priceAndTitle.className = "text-center md:text-left";
+            priceAndTitle.appendChild(title);
+            priceAndTitle.appendChild(price);
+            const card = document.createElement("div");
+            card.className = "md:flex bg-white rounded-lg p-6 hover:bg-gray-300";
+            card.appendChild(image);
+            card.appendChild(priceAndTitle);
+            container.append( card)
             listaElemento.push(container)
         }
     )
-    
     appNode.append(...listaElemento);
 }
 
-nombres();
+nombres()
